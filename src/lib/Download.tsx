dd901d68a220cf6e331064bf9befbacea81eaf03
Phuc Le <lecaobaophuc@gmail.com>
2025-09-09 17:50:52 +0700

    feat: Initial and setup download for Android

---
+++ src/lib/Download.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { invoke } from "@tauri-apps/api/core"
+import { ping, downloadPrivate, downloadPublic, copyFilePath } from '../../../../dist-js';
+import { shareFile } from 'tauri-plugin-share';
+
+function Download() {
+  const [url, setUrl] = useState("https://s3-api-file.mojo.vn/app/98bf4891-4593-4a59-bbb9-cd035a3d25be-yoeq24.png")
+  const [downloadMsg, setDownloadMsg] = useState("")
+
+
+  const handleDownloadPublic = async () => {
+    try {
+      const res = await downloadPublic({ url, fileName: "98bf4891-4593-4a59-bbb9-cd035a3d25be-yoeq24.png", mimeType: "image/png" })
+      setDownloadMsg(res.path || res.uri || "")
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleSharePrivate = async () => {
+    try {
+      const res = await downloadPrivate({ url, fileName: "98bf4891-4593-4a59-bbb9-cd035a3d25be-yoeq24.png" })
+      if (res.path) {
+        await shareFile(res.path as string, "image/png")
+      }
+      setDownloadMsg(res.uri || res.path || "")
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <div className="w-full">
+      <div className="row w-full" style={{ width: '100%' }}>
+        <textarea
+          rows={10}
+          style={{ width: '100%' }}
+          id="greet-input"
+          placeholder="Enter a url...."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+      </div>
+      <div style={{ display: 'flex', gap: 8, marginTop: '10px' }}>
+        <button onClick={handleDownloadPublic}>Download (Public)</button>
+        <button onClick={handleSharePrivate}>Share (Private)</button>
+      </div>
+      <p>{downloadMsg}</p>
+    </div>
+  )
+}
+
+export default Download
